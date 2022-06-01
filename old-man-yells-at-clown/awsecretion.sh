0be7fcca52f3b6bda4685729d035f56a2f3f11f9
ctrlaltreboot <ctrlaltreboot@o-O.local>
2022-06-01 10:29:24 +1000

    Usage dropped non-getops beb...
    Systematic syntax while loop is not forever

--- old-man-yells-at-clown/awsecretion.sh
+++ old-man-yells-at-clown/awsecretion.sh
@@ -1,15 +1,37 @@
 #!/usr/bin/env bash
+set -x
+set -euo pipefail
+IFS=$'\n\t'
 
 WWA=$(basename $0)
 
-OPTION=${1-'list'}
-REGION=${2-'us-west-2'}
-FILTER=${3-'staging'}
-
 usage_drop() {
-    echo "${WWA} <option> <region> <filter>"
+    echo "${WWA} -o<ption> -r<egion>"
     exit 69
 }
+
+# [[ $# -eq 0 ]] && usage_drop
+OPTION='list'
+REGION='us-west-2'
+FILTER='staging'
+
+while getopts ":ho:r:f:" arg; do
+  case $arg in
+    o)
+      OPTION=${OPTARG}
+      ;;
+    r)
+      REGION=${OPTARG}
+      ;;
+    f)
+      FILTER=${OPTARG}
+      ;;
+    h | *) # Display help.
+      usage_drop
+      ;;
+  esac
+done
+
 
 list() {
     aws --region=${REGION} secretsmanager list-secrets --filters Key=name,Values=${FILTER} | jq '.[][] | .Name'  | sort
